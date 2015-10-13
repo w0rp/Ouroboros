e8d2741f32ed1bfa54641622f28300d70743f49f
Luorrak <Luorrak@users.noreply.github.com>
2015-10-13 17:43:12 -0400

    Scroll up hides toolbar, cleaned up empty items

--- app/src/main/java/com/luorrak/ouroboros/util/DragAndDropRecyclerView/OnStartDragListener.java
+++ app/src/main/java/com/luorrak/ouroboros/util/DragAndDropRecyclerView/OnStartDragListener.java
@@ -4,7 +4,7 @@ import android.support.v7.widget.RecyclerView;
 
 /**
  * Ouroboros - An 8chan browser
- * Copyright (C) 2015  NothingOfNote
+ * Copyright (C) 2015  Luorrak
  * <p/>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- app/src/main/java/com/luorrak/ouroboros/util/DragAndDropRecyclerView/WatchListTouchHelper.java
+++ app/src/main/java/com/luorrak/ouroboros/util/DragAndDropRecyclerView/WatchListTouchHelper.java
@@ -2,7 +2,7 @@ package com.luorrak.ouroboros.util.DragAndDropRecyclerView;
 
 /**
  * Ouroboros - An 8chan browser
- * Copyright (C) 2015  NothingOfNote
+ * Copyright (C) 2015  Luorrak
  * <p/>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
