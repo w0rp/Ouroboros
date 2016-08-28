8e86815f766e49997d84b26357df963bc3c69ecc
w0rp <devw0rp@gmail.com>
2016-08-28 21:42:25 +0100

    Merge fix-images

--- app/src/main/java/com/luorrak/ouroboros/util/ChanUrls.java
+++ app/src/main/java/com/luorrak/ouroboros/util/ChanUrls.java
@@ -27,10 +27,28 @@ public class ChanUrls {
     private static final String CATALOG_ENDPOINT = "catalog.json"; //http(s)://siteurl/board/catalog.json
     private static final String THREAD_FOLDER = "res"; //http(s):///siteurl/board/res/threadnumber.json
     private static final String THREAD_ENDPOINT = ".json";
-    private static final String IMAGE_THUMBNAIL_DIRECTORY = "thumb";
-    private static final String IMAGE_DIRECTORY = "src"; //http(s)://siteurl/board/src/tim.ext
+    private static final String OLD_IMAGE_THUMBNAIL_DIRECTORY = "thumb";
+    private static final String IMAGE_THUMBNAIL_DIRECTORY = "file_store/thumb";
+    private static final String OLD_IMAGE_DIRECTORY = "src";
+    private static final String IMAGE_DIRECTORY = "file_store"; //http(s)://siteurl/file_store/tim.ext
     private static final String POST_ENDPOINT = "post.php";
     private static final String DNSBL_ENDPOINT = "dnsbls_bypass.php"; //https://8ch.net/dnsbls_bypass.php
+    private static final int OLD_TIM_MAX_LENGTH = 16;
+
+    /**
+     * Determine if the file uses an older 8chan URL.
+     * There are some files on the site still in existence which must be
+     * supported in this manner.
+     *
+     * @param tim The filename part before the extension.
+     * @return Return true if this a file with an old URL.
+     */
+    private static boolean isOldTim(String tim) {
+        // The current filename scheme uses much larger filenames,
+        // so we can just check the length of this string to detect whether
+        // or not we should use the old path or the new path.
+        return tim.length() <= OLD_TIM_MAX_LENGTH;
+    }
 
     public static String getCatalogUrl(String boardName){
         Uri.Builder builder = new Uri.Builder();
@@ -73,25 +91,60 @@ public class ChanUrls {
         return builder.toString();
     }
 
-    public static String getThumbnailUrl(String boardName, String tim){
+    public static String getThumbnailUrl(String boardName, String tim, String ext){
         Uri.Builder builder = new Uri.Builder();
-        builder.scheme(SCHEME)
+
+        if (
+            !ext.equals(".jpg")
+            && !ext.equals(".jpeg")
+            && !ext.equals(".png")
+            && !ext.equals(".gif")
+        ) {
+            // Only some files retain the same extension.
+            // Otherwise, the thumbnails are JPEGs.
+            ext = ".jpg";
+        }
+
+        if (isOldTim(tim)) {
+            // Some older files use an old path including the board name.
+            // The old images always use JPEG thumbnails, apart from some
+            // special board images we will just ignore.
+            builder.scheme(SCHEME)
                 .authority(DOMAIN_NAME)
                 .appendPath(boardName)
-                .appendPath(IMAGE_THUMBNAIL_DIRECTORY)
+                .appendPath(OLD_IMAGE_THUMBNAIL_DIRECTORY)
                 .appendPath(tim + ".jpg")
                 .build();
+        } else {
+            builder.scheme(SCHEME)
+                .authority(DOMAIN_NAME)
+                .appendPath(IMAGE_THUMBNAIL_DIRECTORY)
+                .appendPath(tim + ext)
+                .build();
+        }
+
         return builder.toString();
     }
 
     public static String getImageUrl(String boardName, String tim, String ext){
         Uri.Builder builder = new Uri.Builder();
-        builder.scheme(SCHEME)
+
+        if (isOldTim(tim)) {
+            // Some older files use an old path including the board name.
+            builder.scheme(SCHEME)
                 .authority(DOMAIN_NAME)
                 .appendPath(boardName)
+                .appendPath(OLD_IMAGE_DIRECTORY)
+                .appendPath(tim + ext)
+                .build();
+        } else {
+            builder.scheme(SCHEME)
+                .authority(DOMAIN_NAME)
                 .appendPath(IMAGE_DIRECTORY)
                 .appendPath(tim + ext)
                 .build();
+        }
+
         return builder.toString();
     }
 
